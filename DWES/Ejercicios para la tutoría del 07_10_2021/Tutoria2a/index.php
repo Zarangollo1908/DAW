f879176991dc1f3c319e0abe52302db2746342e4
Zarangollo1908 <chenchomarin@gmail.com>
2021-11-05 10:59:38 +0100

    Añadido API

--- DWES/Ejercicios para la tutoría del 07_10_2021/Tutoria2a/index.php
+++ DWES/Ejercicios para la tutoría del 07_10_2021/Tutoria2a/index.php
@@ -7,9 +7,14 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
     <head>
         <meta charset="UTF-8">
         <title></title>
+        $headers = [
+            'Authorization: Bearer 4zsaPUYj685B6QOw3rOq4SmrCLr4cQyWgVpsBa8F',
+            'Content-Type: application/json'
+        ];
     </head>
     <body>
         <?php
+       
         // Mostrar los 100 primeros números de la serie de Fibonacci.
         
             echo "0 - 1";
